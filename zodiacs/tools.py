3f11f91e197a37f181a432bf5c8f221ba2145e8b
Szampan <szampan@gmail.com>
2022-05-10 14:36:32 +0200

    Simplify and detach logic from views.py

---
+++ zodiacs/tools.py
@@ -0,0 +1,47 @@
+from datetime import datetime     
+
+class ZodiacSign:
+    def __init__(self, name, start_date, end_date):
+        self.name = name
+        self.start_date = datetime.strptime(start_date, '%Y/%m/%d')
+        self.end_date = datetime.strptime(end_date, '%Y/%m/%d')
+
+    def is_me(self, date):
+        if date.month == 1 and date.day < 20:
+            date = date.replace(year=1901)
+        else:
+            date = date.replace(year=1900)
+
+        return self.start_date < date < self.end_date
+
+ZODIAC_SIGNS = [
+    ZodiacSign('Wodnik', '1900/1/20', '1900/2/18'),
+    ZodiacSign('Ryby', '1900/2/19', '1900/3/20'),
+    ZodiacSign('Baran', '1900/3/21', '1900/4/19'),
+    ZodiacSign('Byk', '1900/4/20', '1900/5/20'),
+    ZodiacSign('Bliźnięta', '1900/5/21', '1900/6/21'),
+    ZodiacSign('Rak', '1900/6/22', '1900/7/22'),
+    ZodiacSign('Lew', '1900/7/23', '1900/8/22'),
+    ZodiacSign('Panna', '1900/8/23', '1900/9/22'),
+    ZodiacSign('Waga', '1900/9/23', '1900/10/22'),
+    ZodiacSign('Skorpion', '1900/10/23', '1900/11/21'),
+    ZodiacSign('Strzelec', '1900/11/22', '1900/12/21'),
+    ZodiacSign('Koziorożec', '1900/12/22', '1901/1/19'),
+    
+]    
+
+
+def get_sign(input):
+    date = datetime.strptime(input, '%d/%m/%Y')
+    for sign in ZODIAC_SIGNS:
+        if sign.is_me(date):
+            return sign.name
+
+def is_input_valid(input):
+    format = '%d/%m/%Y'
+    valid = True
+    try:
+        valid = bool(datetime.strptime(input, format))
+    except ValueError:
+        valid = False
+    return valid
--- zodiacs/views.py
+++ zodiacs/views.py
@@ -1,15 +1,15 @@
-from datetime import datetime     
-
 from django.shortcuts import render
 
 from .models import Sign
+
+from .tools import *
 
 
 def index(request):
     if request.method == 'POST':
         input = request.POST.get('birthday')
         context = {}  
-        db_error_message = 'Ups, coś poszło nie tak...'
+        db_error_message = 'Ups, coś poszło nie tak... '
 
         if '30/02' in input:                                 
             try:
@@ -23,7 +23,7 @@ def index(request):
             try:
                 context['db_sign'] = Sign.objects.get(name=sign)    
             except:
-                context['db_sign'] = db_error_message 
+                context['db_sign'] = db_error_message + sign
         else:
             context['invalid_input'] = 'Nieprawidłowy format daty'   
 
@@ -31,81 +31,14 @@ def index(request):
     return render(request, 'zodiacs/index.html')
 
 
-def is_input_valid(input):
-    format = '%d/%m/%Y'
-    valid = True
-    try:
-        valid = bool(datetime.strptime(input, format))
-    except ValueError:
-        valid = False
-    return valid
+# def is_input_valid(input):
+#     format = '%d/%m/%Y'
+#     valid = True
+#     try:
+#         valid = bool(datetime.strptime(input, format))
+#     except ValueError:
+#         valid = False
+#     return valid
 
 
-def get_sign(input):
-    date = datetime.strptime(input, '%d/%m/%Y')
-    month, day = date.month, date.day
-    if month == 1:
-        if day <= 19:
-            return 'Koziorożec'
-        else:
-            return 'Wodnik'
-    elif month == 2:
-        if day <= 18:
-            return 'Wodnik'
-        else:
-            return 'Ryby'
-    elif month == 3:
-        if day <= 20:
-            return 'Ryby'
-        else:
-            return 'Baran'
-    elif month == 4:
-        if day <= 19:
-            return 'Baran'
-        else:
-            return 'Byk'
-    elif month == 5:
-        if day <= 20:
-            return 'Byk'
-        else:
-            return 'Bliźnięta'
-    elif month == 6:
-        if day <= 20:
-            return 'Bliźnięta'
-        else:
-            return 'Rak'
-    elif month == 7:
-        if day <= 22:
-            return 'Rak'
-        else:
-            return 'Lew'
-    elif month == 8:
-        if day <= 22:
-            return 'Lew'
-        else:
-            return 'Panna'
-    elif month == 9:
-        if day <= 22:
-            return 'Panna'
-        else:
-            return 'Waga'
-    elif month == 10:
-        if day <= 22:
-            return 'Waga'
-        else:
-            return 'Skorpion'
-    elif month == 11:
-        if day <= 21:
-            return 'Skorpion'
-        else:
-            return 'Strzelec'
-    elif month == 12:
-        if day <= 21:
-            return 'Strzelec'
-        else:
-            return 'Koziorożec'
-    else:
-        return 'Invalid month'
     
-
-
